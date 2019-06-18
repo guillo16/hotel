41f20925d94778ea8ff751d6558150fcdee18934
guillermo varela <guillovarela16@gmail.com>
2019-06-17 21:34:23 -0300

    finish swall alert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,19 @@
 import "bootstrap";
 import "../plugins/flatpickr"
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+initSweetalert('#sweet-alert-demo', {
+  title: "Are you sure?",
+  text: "This action cannot be reversed",
+  icon: "warning",
+  buttons: true,
+  dangerMode: true,
+}, (value) => {
+  if (value) {
+    const link = document.querySelector('#delete-link');
+    link.click();
+  }
+  else {
+    swal("Your car is safe!");
+  }
+});
