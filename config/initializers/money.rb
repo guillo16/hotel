32c23747d37ac2c99fd6411b19c26acdff82a327
guillermo varela <guillovarela16@gmail.com>
2019-06-20 10:53:54 -0300

    change monetize initializer

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,6 +1,9 @@
 # encoding : utf-8
+Money.locale_backend = :currency
 
 MoneyRails.configure do |config|
+  config.default_currency = :usd  # or :gbp, :usd, etc.
+
 
   # To set the default currency
   #
